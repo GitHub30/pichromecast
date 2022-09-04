bd18f8218d4b966ea72d840a45713f3141df6c6b
言葉 <funaox@gmail.com>
2022-09-04 16:47:03 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pichromecast',
-    version='0.4',
+    version='0.5',
     description='Library for MicroPython to communicate with the Google Chromecast.',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
